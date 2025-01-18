145220dc26d5c4851963827f2c07e1766eefad08
Yogendra Gothwal <rohitverma43434@gmail.com>
2025-01-18 21:49:40 +0530

    hello world message added to react

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -5,10 +5,9 @@ import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
-
   return (
     <>
-      <h1>Hello ! This is react app</h1>
+      <h1>Hello ! This is react app {count}</h1>
     </>
   );
 }
