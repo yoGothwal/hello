c31577fa9dc37b481d664d8a76bf48fd9720f059
Yogendra Gothwal <rohitverma43434@gmail.com>
2025-01-19 13:57:26 +0530

    added users route

--- backend/index.js
+++ backend/index.js
@@ -7,8 +7,17 @@ const notes = [
         content: "First note",
     }
 ]
+const users = [
+    {
+        name: 'Rohit',
+        username: 'Rohiiii'
+    }
+]
 app.get("/", (req, res) => {
     res.send(`default route`);
+});
+app.get("/users", (req, res) => {
+    res.status(200).send(users);
 });
 app.get("/notes", (req, res) => {
     res.status(200).send(notes)
