1cb99b91c72fbf95b350fc3804506758c3072215
Yogendra Gothwal <rohitverma43434@gmail.com>
2025-01-19 13:17:51 +0530

    added counter and timer

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,13 +1,46 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
+  useEffect(() => {
+    const lastCount = window.localStorage.getItem("count");
+    if (lastCount) {
+      setCount(parseInt(lastCount, 10));
+    }
+  }, []);
+  const handleTimerStart = () => {
+    setInterval(() => {
+      setCount((prev) => {
+        const countTemp = prev + 1;
+        window.localStorage.setItem("count", countTemp);
+        return prev + 1;
+      });
+      console.log("hello");
+    }, 1000);
+  };
   return (
     <>
-      <h1>Hello ! This is react app {count}</h1>
+      <h1>Count : {count}</h1>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+          window.localStorage.setItem("count", count + 1);
+        }}
+      >
+        PLUS
+      </button>
+      <button
+        onClick={() => {
+          setCount(0);
+          window.localStorage.removeItem("count");
+        }}
+      >
+        RESET
+      </button>
+      <button onClick={handleTimerStart}>Start Counter</button>
     </>
   );
 }
